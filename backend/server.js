0073cbb95b0eac042ff8d08eb262753295dcdc4b
Rehman-dev288 <abdulrehmanbhatti288@gmail.com>
2026-02-28 19:17:04 +0500

    Fix: used environment variable for API URL

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,12 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://medicore-three.vercel.app",
+    credentials: true,
+  }),
+);
 
 const JWT_SECRET = process.env.JWT_SECRET || "HS256_SECRET_KEY";
 
